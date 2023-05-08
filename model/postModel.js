5c5165d5d82a48561c83164f17e79ad52e015e95
soban78479 <malik78479@gmail.com>
2023-05-08 10:39:36 +0500

    committed

---
+++ model/postModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+    name: { type: String, default: "abc" },
+    age: { type: Number, min: 18, index: true },
+    bio: { type: String, match: /[a-z]/ },
+    date: { type: Date, default: Date.now },
+    buff: Buffer,
+  });
+  
+  const postModel = mongoose.model("posts", postSchema);
+
+  module.exports = {
+    postModel
+  }
